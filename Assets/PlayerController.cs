66466cefacb84879f47e6dbb659ea76258b693d2
Joshua de Lange <joshua.delange@gmail.com>
2016-12-19 15:48:34 +0100

    first init!

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * -3.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * -3.0f;
+
+        // transform.Rotate(0, x, 0);
+        transform.Translate(x, 0, z);
+    }
+}
