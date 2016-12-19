9006b469740011ea6c7bc7f4d71b306e21a366dd
Joshua de Lange <joshua.delange@gmail.com>
2016-12-19 17:54:03 +0100

    stuff is getting good now

---
+++ Assets/PersonController.cs
@@ -0,0 +1,47 @@
+﻿ using UnityEngine;
+ 
+ public class PersonController: MonoBehaviour
+ {
+     //values that will be set in the Inspector
+     private Transform Target;
+     private float RotationSpeed;
+
+	 Animator animator; 
+
+     //values for internal use
+     private Quaternion _lookRotation;
+     private Vector3 _direction;
+
+     public bool lookAtPlayer;
+     
+     void Start()
+     {
+        animator = GetComponent<Animation>();
+     	lookAtPlayer = false;
+     	RotationSpeed = 2.0f;
+     	Target = GameObject.FindGameObjectsWithTag("Player")[0].transform;
+     }
+
+     // Update is called once per frame
+     void Update()
+     {
+
+     	if(lookAtPlayer){
+
+     		if(animation.IsPlaying("Worshipping")){
+     			animation.Play("LowerHands");
+     		}
+
+	         //find the vector pointing from our position to the target
+	         _direction = (Target.position - transform.position).normalized;
+	 
+	         //create the rotation we need to be in to look at the target
+	         _lookRotation = Quaternion.LookRotation(_direction);
+	 
+	         //rotate us over time according to speed until we are in the required rotation
+	         transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * RotationSpeed);
+
+	     }
+
+     }
+ }
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,12 +2,33 @@
 
 public class PlayerController : MonoBehaviour
 {
-    void Update()
-    {
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * -3.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * -3.0f;
+	void Update()
+	{
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * -3.0f;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * -3.0f;
 
-        // transform.Rotate(0, x, 0);
-        transform.Translate(x, 0, z);
-    }
+		// transform.Rotate(0, x, 0);
+		transform.Translate(x, 0, z);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+
+		if (other.transform.parent.gameObject.CompareTag ("Person"))
+		{
+
+			Debug.Log("OH NO!");
+
+			GameObject[] peopleToRotate = GameObject.FindGameObjectsWithTag("Person");
+
+			Debug.Log(peopleToRotate);
+
+			foreach (GameObject person in peopleToRotate) {
+				Debug.Log("rotating one!");
+				person.GetComponent<PersonController>().lookAtPlayer = true;
+			}
+
+		}
+	}
+
 }
