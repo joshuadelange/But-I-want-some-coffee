b4e34eee5ce0f3d1cf63a9d3286e2e7a7477db6c
Joshua de Lange <joshua.delange@gmail.com>
2016-12-20 19:48:12 +0100

    the animations are made great again

--- Assets/PersonController.cs
+++ Assets/PersonController.cs
@@ -6,40 +6,54 @@
      private Transform Target;
      private float RotationSpeed;
 
-	 Animator animator; 
-
      //values for internal use
      private Quaternion _lookRotation;
      private Vector3 _direction;
 
-     public bool lookAtPlayer;
+     private bool lookAtPlayer;
+
+     private Animator animation;
      
      void Start()
      {
-        animator = GetComponent<Animation>();
-     	lookAtPlayer = false;
-     	RotationSpeed = 2.0f;
-     	Target = GameObject.FindGameObjectsWithTag("Player")[0].transform;
+
+          animation = GetComponent<Animator>();
+
+          lookAtPlayer = false;
+          animation.SetBool("PersonIsStaringAtPlayer", false);
+
+          RotationSpeed = 2.0f;
+          Target = GameObject.FindGameObjectsWithTag("Player")[0].transform;
+     }
+
+     public void startLookingAtPlayer()
+     {
+
+          if(!lookAtPlayer){
+
+     		animation.SetBool("PersonIsStaringAtPlayer", true);
+     		lookAtPlayer = true;
+               
+          }
+
      }
 
      // Update is called once per frame
-     void Update()
+     void LateUpdate()
      {
 
      	if(lookAtPlayer){
 
-     		if(GetComponent<Animation>().IsPlaying("Worshipping")){
-     			GetComponent<Animation>().Play("LowerHands");
-     		}
+               transform.LookAt(Target.position);
 
-	         //find the vector pointing from our position to the target
-	         _direction = (Target.position - transform.position).normalized;
+	         // //find the vector pointing from our position to the target
+	         // _direction = (Target.position - transform.position).normalized;
 	 
-	         //create the rotation we need to be in to look at the target
-	         _lookRotation = Quaternion.LookRotation(_direction);
+	         // //create the rotation we need to be in to look at the target
+	         // _lookRotation = Quaternion.LookRotation(_direction);
 	 
-	         //rotate us over time according to speed until we are in the required rotation
-	         transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * RotationSpeed);
+	         // //rotate us over time according to speed until we are in the required rotation
+	         // transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * RotationSpeed);
 
 	     }
 
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,7 +7,6 @@ public class PlayerController : MonoBehaviour
 		var x = Input.GetAxis("Horizontal") * Time.deltaTime * -3.0f;
 		var z = Input.GetAxis("Vertical") * Time.deltaTime * -3.0f;
 
-		// transform.Rotate(0, x, 0);
 		transform.Translate(x, 0, z);
 	}
 
@@ -17,15 +16,17 @@ public class PlayerController : MonoBehaviour
 		if (other.transform.parent.gameObject.CompareTag ("Person"))
 		{
 
-			Debug.Log("OH NO!");
-
 			GameObject[] peopleToRotate = GameObject.FindGameObjectsWithTag("Person");
 
-			Debug.Log(peopleToRotate);
-
 			foreach (GameObject person in peopleToRotate) {
-				Debug.Log("rotating one!");
-				person.GetComponent<PersonController>().lookAtPlayer = true;
+
+				Debug.Log("rotating");
+				Debug.Log(person);
+
+				PersonController personController = person.GetComponent<PersonController>();
+				if(personController != null){
+					personController.startLookingAtPlayer();
+				}
 			}
 
 		}
